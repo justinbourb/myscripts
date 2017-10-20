33671b6af79361d51a9a0e644b895de6f0751006
justinbourb <justinbourb@gmail.com>
2017-10-20 19:28:56 -0400

    now it works?

--- bulk_create_tiff_from_svs.py
+++ bulk_create_tiff_from_svs.py
@@ -3,18 +3,16 @@
 '''This program will open the files and convert them from .svs to .tiff files
 using the openslide-python module'''
 #reasoning: need tiff files for thumbnails, svs files are too large
-import os, re, openslide
-
+import os, openslide
 
 'open files create thumbnails'
 def openFilesFunc():
     #os.chdir(r'I:\images\need_qc\need_recuts\need_recuts_archive')
-    os.chdir(r'D:\Python36-32\Myscripts\test')
-    for file in os.listdir('.'):
-        try:
-            openslide.OpenSlide(file).get_thumbnail(5000,5000)
-        except:
-            continue
+    #os.chdir(r'D:\Python36-32\Myscripts\test')
+	os.chdir(r'/home/jb/myscripts/test')
+	for file in os.listdir('.'):
+		slide = openslide.OpenSlide(file)
+		slide.get_thumbnail((5000,5000))
 
 
 openFilesFunc()
