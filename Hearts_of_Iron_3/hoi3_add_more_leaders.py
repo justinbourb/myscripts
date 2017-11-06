23db823a405d73ad2693ccdacb7a57d3a3a7a1d0
justin <justinbourb@gmail.com>
2017-11-05 22:25:43 -0500

    finds source leader id and all leader ids from donor files. TODO: need to find and replace leader id and country tags

--- Hearts_of_Iron_3/hoi3_add_more_leaders.py
+++ Hearts_of_Iron_3/hoi3_add_more_leaders.py
@@ -8,15 +8,10 @@ fformat'''
 import os, sys, fileinput, re
 
 
-
-
-def openFilesFunc():
+def source_id_Func(source_id):
     'path and regex definitions'
-    path_to_search = r"D:\Python36-32\MyScripts\test"
+    path_to_search = r"D:\Python36-32\MyScripts\test\source"
     text_to_search = r"(\d{6})"
-    #text_to_search = r'(.*)'
-    leader_id_list = []
-    
 
     'opens and reads file'
     os.chdir(path_to_search)
@@ -27,12 +22,45 @@ def openFilesFunc():
             textfile.close()
             'find regex match (leader_id)'
             match = re.findall(text_to_search, filetext)
-            'create a list of all leader ids to find max # +1 this'
-            'number for additional entries'
-            for i in match:
-                leader_id_list.append(i)
+            'returns last leader_id in source file'
+            'can +1 this number to add new leaders'
+            source_id = match[-1]
+        except:
+                continue
+    return (source_id)
+
+
+def leader_id_list_Func(leader_id_list):
+    'path and regex definitions'
+    path_to_search = r"D:\Python36-32\MyScripts\test\donor"
+    text_to_search = r"(\d{5,6})"
+    total=[]
+    
+
+    'opens and reads file'
+    os.chdir(path_to_search)
+    for file in os.listdir('.'):
+        try:
+            textfile = open(file, 'r')
+            filetext = textfile.read()
+            textfile.close()
+            'creates a list from the regex matches (leader_id)'
+            'from the donor file'
+            leader_id_list = re.findall(text_to_search, filetext)
+            'removes duplicates from the list, sets do not have duplicates'
+            leader_id_list = list(set(leader_id_list))
+            'sorts the list, sets are not sorted'
+            leader_id_list.sort()
+            print(file,":",len(leader_id_list))
         except:
             continue
-        print("This file:",file,"contains these leader_ids:",leader_id_list[-1:])
+    return (leader_id_list)
 
-openFilesFunc()
+
+
+source_id=''
+source_id=source_id_Func(source_id)
+print(source_id)
+leader_id_list=[]
+leader_id_list=leader_id_list_Func(leader_id_list)
+print(leader_id_list)
