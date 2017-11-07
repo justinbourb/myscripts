7c025b9e892a93e98f5263fd04514cccc503fed4
justin <justinbourb@gmail.com>
2017-11-07 08:38:24 -0500

    created dictionary donor_file:donor_leader_id

--- Hearts_of_Iron_3/hoi3_add_more_leaders.py
+++ Hearts_of_Iron_3/hoi3_add_more_leaders.py
@@ -17,13 +17,14 @@ more leaders to the source file (objective of the program).'''
 #to create new names (need name generator) and I don't want to make leaders
 #which are overpowered / too powerful / too skilled which seems likely to
 #happen.  Though perhaps random leader generator is a better approach.
-import os, sys, fileinput, re
+import os, sys, fileinput, re, string
 
 
 def source_id_Func(source_id):
     'path and regex definitions'
     path_to_search = r"D:\Python36-32\MyScripts\test\source"
     text_to_search = r"(\d{6})"
+
 
     'opens and reads file'
     os.chdir(path_to_search)
@@ -37,17 +38,20 @@ def source_id_Func(source_id):
             'returns last leader_id in source file'
             'can +1 this number to add new leaders'
             source_id = match[-1]
+
         except:
                 continue
     return (source_id)
 
 
-def leader_id_list_Func(leader_id_list):
+def donor_file_Func(source_id):
     'path and regex definitions'
     path_to_search = r"D:\Python36-32\MyScripts\test\donor"
     text_to_search = r"(\d{5,6})"
-    total=[]
-    
+    keys=[]
+    values=[]
+    source_id=int(source_id)
+    source_id+=1
 
     'opens and reads file'
     os.chdir(path_to_search)
@@ -58,21 +62,33 @@ def leader_id_list_Func(leader_id_list):
             textfile.close()
             'creates a list from the regex matches (leader_id)'
             'from the donor file'
-            leader_id_list = re.findall(text_to_search, filetext)
+            match = re.findall(text_to_search, filetext)
             'removes duplicates from the list, sets do not have duplicates'
-            leader_id_list = list(set(leader_id_list))
+            match = list(set(match))
             'sorts the list, sets are not sorted'
-            leader_id_list.sort()
-            print(file,":",len(leader_id_list))
+            match.sort()
+            print(file+'pre-match:',match)
+            for i,s in enumerate(match):
+                match[i]=string.replace(match, match[i], source_id,1)
+                source_id+=1
+            print(file+'post-match:',match)
+            'store values for dict creation'
+            keys.append(str(file))
+            values.append(match[1])
+
+
+
+
         except:
             continue
-    return (leader_id_list)
+    'create dictionary to allow backfilling donor files with combined'
+    'source file data'
+    dictionary = dict(zip(keys, values))
+    print(dictionary)
+    return (match)
 
 
 
 source_id=''
 source_id=source_id_Func(source_id)
-print(source_id)
-leader_id_list=[]
-leader_id_list=leader_id_list_Func(leader_id_list)
-print(leader_id_list)
+donor_file_Func(source_id)
