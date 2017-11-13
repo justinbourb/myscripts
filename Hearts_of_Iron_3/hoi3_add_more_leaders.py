ceab92cadccfd01a00340c0aa2e0d09b5d72213d
justin <justinbourb@gmail.com>
2017-11-12 21:57:26 -0500

    this version replaces leader_id in the donor files successfully. TODO: replace country and copy this into the source file. Optional: backfill the donor files from the new source file. Optional2: refactor

--- Hearts_of_Iron_3/hoi3_add_more_leaders.py
+++ Hearts_of_Iron_3/hoi3_add_more_leaders.py
@@ -48,10 +48,12 @@ def donor_file_Func(source_id):
     'path and regex definitions'
     path_to_search = r"D:\Python36-32\MyScripts\test\donor"
     text_to_search = r"(\d{5,6} )"
+    country_name = r"(country = )([A-Z]{3})"
     keys=[]
     values=[]
     source_id=int(source_id)
     source_id+=1
+    line_counter=0
 
     'opens and reads file'
     os.chdir(path_to_search)
@@ -60,22 +62,39 @@ def donor_file_Func(source_id):
             textfile = open(file, 'r')
             filetext = textfile.read()
             textfile.close()
-            'creates a list from the regex matches (leader_id)'
+            for line in fileinput.input(file, inplace=True):
+                text_to_replace = str(source_id)+" "
+                line = re.sub(text_to_search, text_to_replace, line.rstrip())
+                print(line)
+                line_counter+=1
+                '''not 100% accurate but attempts to iterate source_id
+                for every match, not every line'''
+                if (line_counter % 10 == 0):
+                    source_id+=1
+                    line_counter=0
+            ''' this sounds cool: https://stackoverflow.com/questions/15175142/how-can-i-do-multiple-substitutions-using-regex-in-python
+            but I'm just going to repeat my code instead. Lazy/Bad_code_warning=True'''
+
+
+
+            '''creates a list from the regex matches (leader_id)'
             'from the donor file'
-            match = re.findall(text_to_search, filetext)
+            text_to_find = re.findall(text_to_search, filetext)
             'removes duplicates from the list, sets do not have duplicates'
-            match = list(set(match))
+            text_to_find = list(set(text_to_find))
             'sorts the list, sets are not sorted'
-            match.sort()
+            text_to_find.sort()
             'replaces the matched leader_id with next source_id'
             'TODO: write this to the file somehow?'
-            for i,s in enumerate(match):
-                print(source_id)
-                match[i]=source_id
+            for i,s in enumerate(text_to_find):
+                text_to_replace = str(source_id)+" ="
+                for line in fileinput.input(file, inplace=True):
+                    print line.replace(str(text_to_find[i])+" =",text_to_replace )
                 source_id+=1
             'store values for dict creation'
+            '''
             keys.append(str(file))
-            values.append(match[1])
+            values.append(text_to_find[1])
 
 
 
@@ -86,7 +105,7 @@ def donor_file_Func(source_id):
     'source file data'
     dictionary = dict(zip(keys, values))
     print(dictionary)
-    return (match)
+    return ()
 
 
 
