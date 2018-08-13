fc1abe1cf8ea20a3f5003b1c8caf7eb703f3a626
justinbourb <30423126+justinbourb@users.noreply.github.com>
2018-08-12 23:08:35 -0400

    refactored to be slightly more DRY
    Wording changed from "source" and "donor" files to "copy_data_to" and "copy_data_from" which I think is more descriptive.
    File paths now specified in one place, instead of in multiple functions.

--- Hearts_of_Iron_3/hoi3_add_more_leaders.py
+++ Hearts_of_Iron_3/hoi3_add_more_leaders.py
@@ -1,17 +1,17 @@
 #hoi3_add_more_leaders.py
 #11/5/17 written by and copyright Justin Bourbonniere
 '''This program will:
-1)open the "source" file and find the next leader_id to use and
-the "source" file country name
-2) Open all the files in the "donor" directory.  Find and replace
+1)open the "copy_data_to" file and find the next leader_id to use and
+the "copy_data_to" file country name
+2) Open all the files in the "copy_data_from" directory.  Find and replace
 their leader id #'s with the next leader_id number (next leader_id number
 incremented +1 each time).  It will also update the country name from
-whatever it is to the source country name.
-3) It will append the updated donor file to the source file, thus adding
-more leaders to the source file (objective of the program).
-4) Optional objective: backfill the donor files with the updated source file content
-but change the leader id's and country name to the donor file format.
-TODO: make a dictionary to contain donor file: leader_id.  Can find both
+whatever it is to the copy_data_to country name.
+3) It will append the updated copy_data_from file to the copy_data_to file, thus adding
+more leaders to the copy_data_to file (objective of the program).
+4) Optional objective: backfill the copy_data_from files with the updated copy_data_to file content
+but change the leader id's and country name to the copy_data_from file format.
+TODO: make a dictionary to contain copy_data_from file: leader_id.  Can find both
 leader_id and country name (from file name) with this information.'''
 #reasoning: Major nations have upto 900 leaders.  Minor nations have 10-60.
 #I would like to combine minor nation leader lists of countries with
@@ -45,100 +45,105 @@ def fileinput_replace_text_Func(text_to_search, text_to_replace):
             continue
     return(file)
 
-def source_id_Func(source_id,source_country,source_file):
+def copy_data_to_id_Func(copy_data_to_id,copy_data_to_country,copy_data_to_fil, copy_data_to_path_to_search):
     'path and regex definitions'
-    path_to_search = r"D:\Python36-32\MyScripts\test\source"
-    text_to_search = r"(\d{6} )"
+    text_to_search = r"(\d{4,6} )"
     country_to_search = r"(country = [A-Z]{3})"
     'opens and reads file'
-    os.chdir(path_to_search)
+    os.chdir(copy_data_to_path_to_search)
     match,file = oslistdir_and_match_text_Func(text_to_search)
-    'returns last leader_id in source file'
+    'returns last leader_id in copy_data_to file'
     'can +1 this number to add new leaders'
-    source_id = match[-1]
+    copy_data_to_id = match[-1]
     match2 = oslistdir_and_match_text_Func(country_to_search)
-    source_country = list(set(list(match2[0])))[0]
-    source_file = file
+    copy_data_to_country = list(set(list(match2[0])))[0]
+    copy_data_to_file = file
+    print(file)
 
 
-    return (source_id, source_country, source_file)
+    return (copy_data_to_id, copy_data_to_country, copy_data_to_file)
 
 
-def donor_file_Func(source_id,source_country,donor_files):
+def copy_data_from_file_Func(copy_data_to_id,copy_data_to_country,copy_data_from_files, copy_data_from_path_to_search):
     'path and regex definitions'
-    path_to_search = r"D:\Python36-32\MyScripts\test\donor"
     text_to_search = r"(\d{5,6} )"
     country_to_search = r"(country = )([A-Z]{3})"
-    country_to_replace = source_country
+    country_to_replace = "country = "+copy_data_to_country
     keys=[]
     values=[]
-    source_id=int(source_id)
-    source_id+=1
+    copy_data_to_id=int(copy_data_to_id)
+    copy_data_to_id+=1
     line_counter=0
 
     'opens and reads file'
-    os.chdir(path_to_search)
+    os.chdir(copy_data_from_path_to_search)
     for file in os.listdir('.'):
         try:
             for line in fileinput.input(file, inplace=True):
-                text_to_replace = str(source_id)+" "
+                text_to_replace = str(copy_data_to_id)+" "
                 new_line = re.sub(text_to_search, text_to_replace, line.rstrip())
                 print(new_line)
                 line_counter+=1
                 if (line_counter % 10) == 0:
-                    source_id+=1
+                    copy_data_to_id+=1
                     line_counter=0
 
 
         except:
             continue
     file = fileinput_replace_text_Func(country_to_search,country_to_replace)
-    donor_files = file
-    return (donor_files)
+    copy_data_from_files = file
+    return (copy_data_from_files)
 
-def append_files_Func():
+def append_files_Func(copy_data_to_path_to_search, copy_data_from_path_to_search, copy_data_to_country):
     '''TODO: refactor to make this dry, shouldn't hardcode path's.
-    feed this function from donor and source functions'''
-    source_file = open(r"D:\Python36-32\MyScripts\test\source\PER.txt", 'w')
-    for file in os.listdir(r"D:\Python36-32\MyScripts\test\donor"):
-        donor_file = open(file, 'r')
-        source_file.write(donor_file.read())
-        donor_file.close()
-    source_file.close()
+    feed this function from copy_data_from and copy_data_to functions'''
+    file_to_open = copy_data_to_path_to_search+"\\"+copy_data_to_country+".txt"
+    copy_data_to_file = open(file_to_open, 'a')
+    for file in os.listdir(copy_data_from_path_to_search):
+        copy_data_from_file = open(file, 'r')
+        copy_data_to_file.write(copy_data_from_file.read())
+        copy_data_from_file.close()
+    copy_data_to_file.close()
     return()
 
 
 if __name__ == "__main__":
-    source_id=''
-    source_country=''
-    source_file=''
-    donor_files=''
-    source_id,source_country,source_file=source_id_Func(source_id,source_country,source_file)
-    donor_files = donor_file_Func(source_id,source_country, donor_files)
-    append_files_Func()
+    '''declaring variables'''
+    copy_data_to_id=''
+    copy_data_to_country=''
+    copy_data_to_file=''
+    copy_data_from_files=''
+
+    '''specify paths to search (copy to and from):'''
+    copy_data_from_path_to_search = r"C:\Program Files\Python36\MyScripts\test\copy_data_from"
+    copy_data_to_path_to_search = r"C:\Program Files\Python36\MyScripts\test\copy_data_to"
+
+    '''functions to run'''
+    copy_data_to_id,copy_data_to_country,copy_data_to_file=copy_data_to_id_Func(copy_data_to_id,copy_data_to_country,copy_data_to_file, copy_data_to_path_to_search)
+    copy_data_from_files = copy_data_from_file_Func(copy_data_to_id,copy_data_to_country, copy_data_from_files, copy_data_from_path_to_search)
+    append_files_Func(copy_data_to_path_to_search, copy_data_from_path_to_search, copy_data_to_country)
 
 
 '''creates a list from the regex matches (leader_id)'
-'from the donor file'
+'from the copy_data_from file'
 text_to_find = re.findall(text_to_search, filetext)
 'removes duplicates from the list, sets do not have duplicates'
 text_to_find = list(set(text_to_find))
 'sorts the list, sets are not sorted'
 text_to_find.sort()
-'replaces the matched leader_id with next source_id'
+'replaces the matched leader_id with next copy_data_to_id'
 'TODO: write this to the file somehow?'
 for i,s in enumerate(text_to_find):
-    text_to_replace = str(source_id)+" ="
+    text_to_replace = str(copy_data_to_id)+" ="
     for line in fileinput.input(file, inplace=True):
         print line.replace(str(text_to_find[i])+" =",text_to_replace )
-    source_id+=1
+    copy_data_to_id+=1
 'store values for dict creation'
-
 keys.append(str(file))
 values.append(text_to_find[1])
-
-'create dictionary to allow backfilling donor files with combined'
-'source file data'
+'create dictionary to allow backfilling copy_data_from files with combined'
+'copy_data_to file data'
 dictionary = dict(zip(keys, values))
 print(dictionary)
 '''
